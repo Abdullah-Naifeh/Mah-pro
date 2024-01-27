3f61ed911c9ec3c8290b1a051d5b3fa7d8adfe55
unknown <abdna59@gmail.com>
2024-01-27 13:06:20 +0300

    fix phone view responsive

--- app/page.js
+++ app/page.js
@@ -1,16 +1,18 @@
+import Image from "next/image";
 import Navbar from "../components/Navbar";
+import img from "../public/person.png"
 import "@/styles/main.css"
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <main className="flex h-[91vh]">
-        <div className="w-[30%] bg-[#3CB2C9]">
-          <img src=""></img>
+      <main className="flex flex-col md:flex-row h-[91vh]">
+        <div className="h-[100%] w-[100%] md:h-[100%] md:w-[30%] bg-[#3CB2C9]">
+          <Image className="object-contain h-[100%]" src={img}></Image>
         </div>
-        <div className="w-[70%]">
-          <div className="flex flex-col justify-center items-start h-[91vh] px-24">
+        <div className="w-[100%] md:w-[70%]">
+          <div className="flex flex-col justify-start md:justify-center items-start md:h-[91vh] px-5 py-8 md:px-24">
             <h5 className="yellowCircle text-[14px] leading-5 font-semibold md:text-[18px]"></h5>
             <h1 className="blueCircle text-[30px] leading-5 font-semibold my-[10px] md:text-[60px] md:leading-[72px] md:font-bold text-[#292929]">I AM M.MAHJOUB</h1>
             <p className="text-[18px] leading-8 text-[#878c9f] font-medium">A SUDANESE INVENTOR</p>
